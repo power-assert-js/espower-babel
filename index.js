62103eb21deae71c7166c8e46f4c3a16940d6971
azu <azuciao@gmail.com>
2015-02-16 08:05:09 +0900

    chore(npm): use babel instead of 6to5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-var to5 = require('6to5-core');
+var babel = require('babel-core');
 var fs = require("fs");
 var minimatch = require('minimatch');
 var extend = require('xtend');
@@ -7,13 +7,13 @@ var convert = require('convert-source-map');
 var espowerSource = require('espower-source');
 var extensions = require.extensions,
     originalLoader = extensions['.js'];
-function espower6to5(options) {
+function espowerBabel(options) {
     var separator = (options.pattern.lastIndexOf('/', 0) === 0) ? '' : '/',
         pattern = options.cwd + separator + options.pattern;
 
     extensions['.js'] = function (localModule, filepath) {
         if (minimatch(filepath, pattern)) {
-            var result5 = to5.transform(fs.readFileSync(filepath, 'utf-8'), {filename: filepath});
+            var result5 = babel.transform(fs.readFileSync(filepath, 'utf-8'), {filename: filepath});
             var resultCode = espowerSource(result5.code, filepath, extend(options.espowerOptions, {sourceMap: result5.map}));
             localModule._compile(resultCode, filepath);
         } else {
@@ -22,4 +22,4 @@ function espower6to5(options) {
     };
 }
 
-module.exports = espower6to5;
+module.exports = espowerBabel;
