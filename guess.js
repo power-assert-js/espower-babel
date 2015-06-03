9b5e03b9b7a37ca86f12c45a7dbf71e67a978869
59naga <i59naga@icloud.com>
2015-06-03 18:53:18 +0900

    Support `mocha --compilers es6:espower-babel/guess`

--- guess.js
+++ guess.js
@@ -3,13 +3,24 @@ var path = require('path'),
     pattern = 'test/**/*.js',
     packageData,
     testDir,
-    babelrc;
+    babelrc,
+    extension = '.js';
+
+// Override extension via `mocha --compilers <extension>:espower-babel/guess`
+process.argv.forEach(function(arg){
+    if (arg.indexOf(':espower-babel/guess') === -1) {
+        return;
+    }
+
+    extension = '.'+arg.split(':')[0];
+});
+
 packageData = require(path.join(process.cwd(), 'package.json'));
 if (packageData &&
     typeof packageData.directories === 'object' &&
     typeof packageData.directories.test === 'string') {
     testDir = packageData.directories.test;
-    pattern = testDir + ((testDir.lastIndexOf('/', 0) === 0) ? '' : '/') + '**/*.js';
+    pattern = testDir + ((testDir.lastIndexOf('/', 0) === 0) ? '' : '/') + '**/*'+extension;
 }
 
 babelrc = resolveBabelrc(process.cwd(), {});
@@ -17,5 +28,6 @@ babelrc = resolveBabelrc(process.cwd(), {});
 require('./index')({
     cwd: process.cwd(),
     pattern: pattern,
-    babelrc: babelrc
+    babelrc: babelrc,
+    extension: extension
 });
