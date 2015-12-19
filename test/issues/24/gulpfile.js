f8192da0ac29d1352f4cb06c3a0a2590e9dd7fd2
nodaguti <nodaguti@gmail.com>
2015-12-19 23:25:25 +0900

    Move gulpfile.js to under test/issues/24

--- test/issues/24/gulpfile.js
+++ test/issues/24/gulpfile.js
@@ -6,7 +6,7 @@ var mocha = require('gulp-mocha');
 
 // Since the `--compiers` option is not available in gulp-mocha,
 // registering a require hook is needed to make Mocha work with espower-babel.
-require('./guess');
+require('../../../guess');
 
 gulp.task('test:issue-#24', function() {
   // This gulp task is expected to run a espowered Mocha test successfully.
--- test/issues/24/index.js
+++ test/issues/24/index.js
@@ -3,7 +3,13 @@ import { spawn } from 'child_process';
 
 describe('Gulp', () => {
   it('can run a task whose name contains a colon', (done) => {
-    const gulp = spawn('gulp', ['test:issue-#24']);
+    const gulp = spawn('gulp', [
+      '--gulpfile',
+      './test/issues/24/gulpfile.js',
+      '--cwd',
+      '.',
+      'test:issue-#24'
+    ]);
 
     gulp.on('exit', (code) => {
       assert(code === 0);
